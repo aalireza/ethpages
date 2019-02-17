98fd9e58d987aaea86fa73080db70d8c5ec37543
Alireza Rafiei <mail@rafiei.net>
2019-02-17 10:20:39 -0800

    thegraph for the new chain

--- graph/mapping.ts
+++ graph/mapping.ts
@@ -1,22 +1,10 @@
-import { NewGravatar, UpdatedGravatar } from './types/Gravity/Gravity'
-import { Gravatar } from './types/schema'
+import { UserDataUpdated } from './types/Keybook/Keybook'
+import { User } from './types/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
-
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleNewUsers(event: UserDataUpdated): void {
+    let user = new User(event.params.email)
+    user.email = event.params.email
+    user.name = event.params.name
+    user.telegram = event.params.telegram
+    user.save()
 }
