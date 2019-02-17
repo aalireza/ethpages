69e0dc759d8452e0b47881a68dc3c2802ef11594
Kevin Baragona <kmbaragona@gmail.com>
2019-02-16 19:22:22 -0700

    make signing and recover actually worjk

--- js_signer/sign.js
+++ js_signer/sign.js
@@ -20,14 +20,10 @@ var eth = web3.eth;
         var dataToSign =  web3.utils.soliditySha3(userAddress, web3.utils.toHex("email: "), web3.utils.toHex(email));
         console.log('datatosign: '+dataToSign);
 
-        var h = web3.utils.soliditySha3(dataToSign);
-        //var h = dataToSign;
-        var sig = (await web3.eth.sign(h, signer)).slice(2);
-        var r = `0x${sig.slice(0, 64)}`
-        var s = `0x${sig.slice(64, 128)}`
-        var v = web3.utils.toDecimal(sig.slice(128, 130)) + 27
+        var h = dataToSign;
+        var sig = (await web3.eth.sign(h, signer));
+        console.log('got signature:', sig);
 
-        console.log(`got signature: r:${r} s:${s} v:${v}`);
         process.exit();
 
     }catch(e){
