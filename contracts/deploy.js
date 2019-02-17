d3cc7050d937eec621ad8cabd54650fab704d2cb
sashavol <sashavol@sashavol.com>
2019-02-17 02:44:09 -0700

    Got rid of truffle.

---
+++ contracts/deploy.js
@@ -0,0 +1,34 @@
+var Web3 = require('web3');
+var process = require('process');
+var fs = require('fs');
+
+function usage() {
+    console.error('Usage: deploy FROM_ADDRESS [GETH_URL]');
+    process.exit(1);
+}
+
+var from = process.argv[2];
+if(from === undefined) {
+    usage();
+}
+
+var url = process.argv[3] || 'http://localhost:8545';
+var web3 = new Web3(url);
+
+var guestbookInfo = JSON.parse(fs.readFileSync('Keybook.json'));
+var contractInfo = guestbookInfo.contracts['Keybook.sol:Keybook'];
+
+var abi = JSON.parse(contractInfo.abi);
+var contract = new web3.eth.Contract(abi);
+
+contract.deploy({
+    data: '0x' + contractInfo['bin']
+}).send({
+    from: from,
+    gas: 5e6,
+    gasPrice: 1e9
+}).on('receipt', function(receipt) {
+    console.error('receipt:', receipt);
+}).catch(function(err) {
+    console.error(err);
+});
